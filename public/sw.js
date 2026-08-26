8a1914dde6a60c480664bc52f6a4f6c25e0067cc
Krzysztof <krzjur@gmail.com>
2026-08-26 06:56:23 +0200

    refactor: remove service worker fetch event listener
    Cleanup unused fetch event handler and improve code formatting in the service worker registration logic.

--- public/sw.js
+++ public/sw.js
@@ -4,13 +4,11 @@ self.addEventListener('install', () => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
       .then(() => self.registration.unregister())
       .then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
 
